31b3ec992277917ae39e23a4c2de3734ed04efd9
winnie0521 <wenyue.xing@brown.edu>
2018-03-06 10:57:58 -0500

    add coverage

---
+++ R/coverage.R
@@ -0,0 +1,24 @@
+#' coverage rate: mean, median and sd
+#'
+#' @param bamfile
+#' @return coverage rate mean, median and sd matrix
+
+
+
+coverage <- function(bamfile){
+
+
+  p_cover <- ScanBamParam(what=scanBamWhat(),flag=scanBamFlag(isUnmappedQuery = FALSE))
+  bam = scanBam(bamfile, param=p_cover)[[1]]
+
+
+  I_ranges <- IRanges(start=bam$pos, width=bam$qwidth, names=make.names(bam$qname, unique=TRUE))
+  G_ranges <- GRanges(seqnames=Rle(bam$rname), ranges=I_ranges, strand=Rle(bam$strand), flag=bam$flag, readid=bam$rname)
+
+  coverage <- rbind(mean(coverage(G_ranges)),median(coverage(G_ranges)),sd(coverage(G_ranges)))
+  rownames(coverage) <- c("mean","median","sd")
+
+  return(coverage)
+
+
+}
