d4bdf3976e814975d14d1051842e65ca8ed23194
winnie0521 <wenyue.xing@brown.edu>
2018-03-06 14:05:12 -0500

    rate

--- R/mult_map.R
+++ R/mult_map.R
@@ -1,12 +1,18 @@
 #' multiple mapping
 #'
 #' @param bamfile the path to bam file
+#' @param quality set default of mapping quality standard to 30
 
 
-mult_map <- function(bamfile){
+mult_map <- function(bamfile,quality=30,option){
 p_mm <- ScanBamParam(what=scanBamWhat(),flag=scanBamFlag(isUnmappedQuery = FALSE))
 mult_map_r = scanBam(bamfile, param=p_mm)[[1]]
 
-mult_map  <- lapply(mult_map_r, function(x)x[mult_map_r$mapq<=30])
-return(mult_map)
+mult_map  <- lapply(mult_map_r, function(x)x[mult_map_r$mapq<=quality])
+
+length <- bam_length(bamfile)
+
+if (option == "count") return(length(mult_map$qname))
+if (option == "rate") return(length(mult_map$qname)/length)
+
 }
--- R/unq_mapping.R
+++ R/unq_mapping.R
@@ -1,13 +1,17 @@
 #' unique mapping
 #'
 #' @param bamfile the path to bam file
+#' @param quality set default to 30
 
 
-unq_map <- function(bamfile){
+unq_map <- function(bamfile,quality=30,option){
 
   p_um <- ScanBamParam(what=scanBamWhat(),flag=scanBamFlag(isUnmappedQuery = FALSE))
   uniq_map_r = scanBam(bamfile, param=p_um)[[1]]
 
-  uniq_map  <- lapply(uniq_map_r, function(x)x[uniq_map_r$mapq>30])
-  return(uniq_map)
+  uniq_map  <- lapply(uniq_map_r, function(x)x[uniq_map_r$mapq>quality])
+
+  length <- bam_length(bamfile)
+  if (option == "count") return(length(uniq_map$qname))
+  if (option == "rate") return(length(uniq_map$qname)/length)
 }
